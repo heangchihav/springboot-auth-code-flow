0eb74b4c940222ebf08f3eb490a962aee51ec5d3
Heang Chihav <heangchihav@gmail.com>
2026-04-02 13:38:01 +0700

    restructure project into multi-service setup, move auth-code-flow into services and add pure-client & resource services

---
+++ pure-client/src/main/java/com/demo/pure_client/PureClientApplication.java
@@ -0,0 +1,13 @@
+package com.demo.pure_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PureClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PureClientApplication.class, args);
+	}
+
+}
